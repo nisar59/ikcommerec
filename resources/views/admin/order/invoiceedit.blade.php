882495ecd7f411c7bdffa6395a6b2715770339f9
shehryar <shehryar@websouls.com>
2021-02-15 16:51:22 -0800

    invoice styling

--- resources/views/admin/order/invoiceedit.blade.php
+++ resources/views/admin/order/invoiceedit.blade.php
@@ -8,8 +8,102 @@
 @endsection
 @section('content')
 
+
+
+
+<div class="clearfix header">
+      <div id="logo">
+        <img src="logo.png">
+      </div>
+      <div id="company">
+        <h2 class="name">Company Name</h2>
+        <div>455 Foggy Heights, AZ 85004, US</div>
+        <div>(602) 519-0450</div>
+        <div><a href="mailto:company@example.com">company@example.com</a></div>
+      </div>
+      </div>
+    </div>
+    <main>
+      <div id="details" class="clearfix">
+        <div id="client">
+          <div class="to">INVOICE TO:</div>
+          <h2 class="name">John Doe</h2>
+          <div class="address">796 Silver Harbour, TX 79273, US</div>
+          <div class="email"><a href="mailto:john@example.com">john@example.com</a></div>
+        </div>
+        <div id="invoice">
+          <h1>INVOICE 3-2-1</h1>
+          <div class="date">Date of Invoice: 01/06/2014</div>
+          <div class="date">Due Date: 30/06/2014</div>
+        </div>
+      </div>
+      <table border="0" cellspacing="0" cellpadding="0">
+        <thead>
+          <tr>
+            <th class="no">#</th>
+            <th class="desc">DESCRIPTION</th>
+            <th class="unit">UNIT PRICE</th>
+            <th class="qty">QUANTITY</th>
+            <th class="total">TOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="no">01</td>
+            <td class="desc"><h3>Website Design</h3></td>
+            <td class="unit">$40.00</td>
+            <td class="qty">30</td>
+            <td class="total">$1,200.00</td>
+          </tr>
+          <tr>
+            <td class="no">02</td>
+            <td class="desc"><h3>Website Development</h3></td>
+            <td class="unit">$40.00</td>
+            <td class="qty">80</td>
+            <td class="total">$3,200.00</td>
+          </tr>
+          <tr>
+            <td class="no">03</td>
+            <td class="desc"><h3>Search Engines Optimization</h3></td>
+            <td class="unit">$40.00</td>
+            <td class="qty">20</td>
+            <td class="total">$800.00</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="1">SUBTOTAL</td>
+            <td>$5,200.00</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="1">Delivery / Charges</td>
+            <td>$1,300.00</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="1">Discount</td>
+            <td>$1,300.00</td>
+          </tr>
+          <tr>
+            <td colspan="3"></td>
+            <td colspan="1">GRAND TOTAL</td>
+            <td>$6,500.00</td>
+          </tr>
+        </tfoot>
+      </table>
+    </main>
+    <br/>
+    <br/>
+    <br/>
+
+
+
+
+
     <div class="row align-items-center">
-        <div class="col-sm-6">
+        <div class="col-md-6">
             <div class="page-title-box">
                 <h4 class="font-size-18">Invoice Management</h4>
                 <ol class="breadcrumb mb-0">
@@ -42,27 +136,10 @@
 
 
                 <!-- Nav tabs -->
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#general" role="tab">
-                            <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
-                            <span class="d-none d-sm-block">General</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link " data-toggle="tab" href="#categories" role="tab">
-                            <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
-                            <span class="d-none d-sm-block">Prodcuts</span>
-                        </a>
-                    </li>
-
-
-                </ul>
 
                 <!-- Tab panes -->
                 <div class="tab-content">
-                    <div class="tab-pane active p-3" id="general" role="tabpanel">
+                    <div class="tab-pane active p-3">
 
                         <div class="row">
                             <div class="col-xs-6 col-sm-6 col-md-6">
@@ -92,23 +169,7 @@
 
                                 </div>
                             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        </div>
-                    </div>
-                    <div class="tab-pane p-3" id="categories" role="tabpanel">
-                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
 
 
 
@@ -161,7 +222,18 @@
 
 
 
+
+
+
+
+
+
+
+
+
+                        </div>
                     </div>
+                    
 
 
 
