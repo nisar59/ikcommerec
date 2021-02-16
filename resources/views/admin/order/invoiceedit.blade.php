84659a3cc4cf8e3680d054de1c02e70802e2ff61
Nisar <nisar@websouls.com>
2021-02-16 10:37:21 +0500

    invoice design

--- resources/views/admin/order/invoiceedit.blade.php
+++ resources/views/admin/order/invoiceedit.blade.php
@@ -8,28 +8,25 @@
 @endsection
 @section('content')
 
-
-
-
+<div class="container mt-5">
+<div class="well" style="border: 1px solid #626ed4;">
+    <div class="well text-center" style="border-bottom: 1px solid #626ed4; background: #626ed4; color: white;"><h1>INVOICE</h1></div>
 <div class="clearfix header">
       <div id="logo">
-        <img src="logo.png">
+        <img src="{{ url('public/uploads/images/'.settingsValue('config_site_logo'))}}" alt="" height="22">
       </div>
       <div id="company">
-        <h2 class="name">Company Name</h2>
+        <h2 class="name">{{settingsValue('config_site_title')}}</h2>
         <div>455 Foggy Heights, AZ 85004, US</div>
         <div>(602) 519-0450</div>
-        <div><a href="mailto:company@example.com">company@example.com</a></div>
       </div>
       </div>
-    </div>
     <main>
       <div id="details" class="clearfix">
         <div id="client">
           <div class="to">INVOICE TO:</div>
           <h2 class="name">John Doe</h2>
-          <div class="address">796 Silver Harbour, TX 79273, US</div>
-          <div class="email"><a href="mailto:john@example.com">john@example.com</a></div>
+          <div class="address">{{$data['user']->billing_address_1}},{{$data['user']->billing_city}}, ,{{$data['user']->post_code}}</div>
         </div>
         <div id="invoice">
           <h1>INVOICE 3-2-1</h1>
@@ -94,161 +91,8 @@
         </tfoot>
       </table>
     </main>
-    <br/>
-    <br/>
-    <br/>
-
-
-
-
-
-    <div class="row align-items-center">
-        <div class="col-md-6">
-            <div class="page-title-box">
-                <h4 class="font-size-18">Invoice Management</h4>
-                <ol class="breadcrumb mb-0">
-                    <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Dashboard</a></li>
-
-                    <li class="breadcrumb-item active">View Invoice: INV-{{$data['user']->id}} </li>
-                </ol>
-            </div>
-        </div>
-        @if (count($errors) > 0)
-            <div class="row">
-                <div class="col-12">
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div></div>
-        @endif
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-
-                {!! Form::model($data['user'], ['method' => 'POST','url' => ['admin/ordered/update/'.$data['user']->id],"enctype"=>"multipart/form-data"]) !!}
-                <h4 class="card-title">Invoice INV-{{$data['user']->id}}</h4>
-
-
-                <!-- Nav tabs -->
-
-                <!-- Tab panes -->
-                <div class="tab-content">
-                    <div class="tab-pane active p-3">
-
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6">
-                                <div class="form-group">
-                                    <strong>Name: {{$data['user']->first_name}} {{$data['user']->last_name}}<br />{{$data['user']->billing_email}}
-                                    <br />
-                                        {{$data['user']->billing_phone}}
-                                        <br />
-                                        Billing Address:
-                                        <br />
-                                        {{$data['user']->billing_address_1}},{{$data['user']->billing_city}}, ,{{$data['user']->post_code}}
-                                    </strong>
-
-                                </div>
-                            </div>
-                            <div class="col-xs-6 col-sm-6 col-md-6">
-                                <div class="form-group">
-                                    <strong>Shipping Address:
-                                        <br />
-                                        Name: {{$data['user']->ship_full_name}} <br />{{$data['user']->ship_email}}
-                                        <br />
-                                        {{$data['user']->billing_phone}}
-                                        <br />
-
-                                        {{$data['user']->ship_address_1}},{{$data['user']->ship_city}},{{$data['user']->ship_state}},{{$data['user']->ship_postal_code}}
-                                    </strong>
-
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-
-
-
-
-                            <div class="form-group">
-                                <table id="datatable" class="table table-striped ">
-                                    <thead>
-                                    <tr>
-
-                                        <th>Product Name</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th>Total</th>
-
-                                        {{--<th>Sub Categories</th>--}}
-
-
-
-
-                                    </tr>
-                                    </thead>
-                                  
-
-                                    <tbody>
-                                   
-                                    @foreach ($data['user']->orderProducts as $key => $userPro)
-                                     @php
-                                    $pname=Modules\Products\Entities\Products::where('id',$userPro->product_id)->pluck('name')->first();
-                                    @endphp
-                                        <tr> 
-                                            <td>{{$pname}}</td>
-                                            <td>{{$userPro->price}} </td>
-                                            <td>{{$userPro->quantity}} </td>
-                                            <td>{{$userPro->total_price}} </td>
-
-
-                                            {{--<td>{{$user->first_name}} {{$user->last_name}}</td>--}}
-
-
-
-                                        </tr>
-                                    @endforeach
-
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        </div>
-                    </div>
-                    
-
-
-
-                </div>
-                {{--<div class="col-xs-12 col-sm-12 col-md-12 text-center">--}}
-                    {{--<button type="submit" class="btn btn-primary">Update</button>--}}
-                {{--</div>--}}
-                {!! Form::close() !!}
-
-            </div>
-        </div>
-
-
-    </div> <!-- end col -->
-    </div> <!-- end row -->
+ </div>
+</div>
 @endsection
 @section('script')
     <script src="{{ URL::asset('assets/libs/select2/js/select2.min.js')}}"></script>
